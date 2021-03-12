8a9749648e09ef36276f0e32e64ce25b8000fd80
Juan Guzman <juan.guzman.sd@gmail>
2021-03-12 15:33:00 -0600

    deploy & README

--- src/Board.js
+++ src/Board.js
@@ -100,9 +100,7 @@ function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.25 }) {
   return (
     <>
       <h1>Lights Out App</h1>
-      <p><small>Lights Out is a logic/puzzle game, played on a gird of individual lights, which can either be lit or unlit. The puzzle is won when when all of the lights are turned off.
-        <br></br>
-You can click on a cell to toggle that light — but it also toggles the light above it, to the left of it, to the right of it, and below it. (Cells on an edge or in the corner won’t flip as many lights, since they are missing some neighbors).</small></p>
+      <p><small>Lights Out is a logic/puzzle game, played on a gird of individual lights, which can either be lit or unlit. The puzzle is won when when all of the lights are turned off. You can click on a cell to toggle that light — but it also toggles the light above it, to the left of it, to the right of it, and below it. (Cells on an edge or in the corner won’t flip as many lights, since they are missing some neighbors).</small></p>
       <table className="Board">
         <tbody>{tblBoard}</tbody>
       </table>
